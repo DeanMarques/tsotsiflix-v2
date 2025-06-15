9f593b9975a5ef28e0129f76203579224e6ac5f4
DeanMarques <deanmarques9430@gmail.com>
2025-06-15 13:30:19 +0200

    initial

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -213,33 +213,24 @@ class MovieController extends Controller
         $destinationDir = '/mnt/usb/tsotsiflix/mediafiles/movies/';
 
         try {
-            // Get all directories in the source folder
             $movieDirs = array_filter(glob($sourceDir . '*'), 'is_dir');
 
             foreach ($movieDirs as $movieDir) {
-                // Get the movie folder name (which will be the new filename)
                 $folderName = basename($movieDir);
-                
-                // Find the mp4 file in the movie directory
                 $mp4Files = glob($movieDir . '/*.mp4');
                 
                 if (!empty($mp4Files)) {
-                    $sourceFile = $mp4Files[0]; // Take the first mp4 file found
+                    $sourceFile = $mp4Files[0];
                     $newFilename = $folderName . '.mp4';
                     $destinationFile = $destinationDir . $newFilename;
 
-                    // Check if destination directory exists, create if not
-                    // if (!is_dir($destinationDir)) {
-                    //     mkdir($destinationDir, 0755, true);
-                    // }
-
-                    // Move and rename the file
-                    if (rename($sourceFile, $destinationFile)) {
-                        // Remove the now empty source directory
-                        rmdir($movieDir);
-                        Log::info("Successfully moved and renamed: {$folderName}");
+                    // Copy the file first
+                    if (copy($sourceFile, $destinationFile)) {
+                        // Use shell_exec to remove the entire directory and its contents
+                        shell_exec("rm -rf " . escapeshellarg($movieDir));
+                        Log::info("Successfully processed: {$folderName}");
                     } else {
-                        Log::error("Failed to move file: {$folderName}");
+                        Log::error("Failed to copy file: {$folderName}");
                     }
                 } else {
                     Log::warning("No MP4 file found in directory: {$folderName}");
